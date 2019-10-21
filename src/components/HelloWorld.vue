501296858ed3760414f6f2c040e4ed1bd9127af0
Nesrine <outemzabet@et.esiea.fr>
2019-10-21 23:29:18 +0200

    page d'accueil

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-center wrap row justify-center align-center>
+      <v-img height="100%" width="150%" :src="require('../assets/ko.png')">
+        <v-layout row justify-center align-center>
+          <v-container grid-list-xl pt-5 style="max-height: 10em;">
+            <v-layout row justify-center align-center>
+              <v-card color="rgb(255, 0, 0, 0.16)" class="text-center lighten-5">
+                <v-card-title class="section__title justify-center">
+                  
+                    <v-flex mb-4>
+                      <h1 class="display-2 font-weight-bold mb-3">Welcome to Quizzland</h1>
+                      <p class="subheading font-weight-regular">
+                        Want to have fun with a multitude of tests concocted by our team ?
+                        <br />Let's join us !
+                      </p>
+                    </v-flex>
+                </v-card-title>
+              </v-card>
+            </v-layout>
+          </v-container>
+        </v-layout>
+
+        <div>
+          <v-card :elevation="card" class="mx-auto" :width="width">
+            <v-img
+              v-if="media"
+              class="black--text"
+              height="200px"
+              width="400"
+              :src="require('../assets/tra.png')"
+            >
+            </v-img>
+
+            <v-card-text>Quelle est la saison qui correspond le mieux à votre personnalité profonde ?</v-card-text>
+            <v-card-actions v-if="actions">
+              <v-btn icon>
+                <v-icon class="mx-2" fab dark normal color="green">mdi-flower</v-icon>
+              </v-btn>
+
+              <v-btn outlined color="primary" @click="redirect">Faire le test</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+
+        <div>
+          <div id="surveyElement"></div>
+          <div id="surveyResult"></div>
+        </div>
+        <p class="text-center" style="color:#030200;">
+          <br />For always more fun →
+          <a
+            href="https://www.openask.com/fr/tests/"
+            style="color:#10257e;"
+            target="_blank"
+          >Click here 😊</a>
+        </p>
+        
+      </v-img>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+//import Quizz from "./Quizz";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data: () => ({
+    media: true,
+    actions: true,
+    width: 344,
+    height: undefined,
+//    showQuizz: false
+  }),
+//  components: { Quizz },
+  methods: {
+//    redirect() {
+//      this.$router.push("/quizz");
+    }
+  //}
+};
 </script>
+    
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
