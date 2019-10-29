cc70ad3412e2c369176579a33dabb51414d1523b
Nesrine <outemzabet@et.esiea.fr>
2019-10-29 19:41:57 +0100

    questions test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -73,6 +73,14 @@ export default {
         ]
       ],
       [
+        ["What color do you represent ?"],[
+          ["Bleu","https://cuirenstock.com/6209-large_default/peau-de-cuir-d-agneau-metallise-bleu.jpg",1],
+          ["Orange","https://cdn.pixabay.com/photo/2011/09/14/14/47/orange-9283_960_720.jpg",2],
+          ["Vert","https://www.careandcoaching.nl/wp-content/uploads/2017/03/green-leaves-on-green-background.jpg",3],
+          ["Rose","https://cdn11.bigcommerce.com/s-19ea7/images/stencil/1280x1280/products/191/501/Teaberry_Rose_Dry_Blusher__70069.1332299622.jpg?c=2&imbypass=on",4]
+        ]
+      ],
+      [
         ["Which flower do you prefer ?"],[
           ["Edelweiss","https://img-3.journaldesfemmes.fr/zSqNG947ybV9QlLEdrRtuXTrNJk=/910x607/smart/68ac12ed12f84f99bc0759fdc408dee2/ccmcms-jdf/10960187.jpg",1],
           ["Tulip","https://media.ooreka.fr/public/image/plant/80/mainImage-source-9412304.jpg",2],
@@ -87,7 +95,32 @@ export default {
           ["Thaiti","http://www.nolobike.com/wp-content/uploads/2017/06/conseils-voyage-tahiti-1200x900.jpg",3],
           ["Cappadocia","https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1496699939/hot-air-balloons-festival-cappadocia-turkey-HOTAIR0605.jpg?itok=WZ9CVr-K",4]
         ]
-      ]
+      ],
+    [
+        ["What is the most delicious fruit ?"],[
+          ["Ananas","https://www.lanutrition.fr/sites/default/files/styles/article_large/public/ressources/shutterstock_572083087_3.jpg?itok=813o9KfI",1],
+          ["Poire","https://i-reg.unimedias.fr/sites/art-de-vivre/files/styles/large/public/r69-poire-williams_fotolia.jpg",2],
+          ["Pastèque","https://www.lanutrition.fr/sites/default/files/styles/article_large/public/ressources/pasteque_morceaux.jpg?itok=snxfV3VQ",3],
+          ["Fraise","https://www.sante-sur-le-net.com/wp-content/uploads/2018/07/fraise-sante.jpg",4]
+        ]
+      ],
+      [
+        ["Which quote defines you best ?"],[
+          ["Coluche","https://citation-celebre.leparisien.fr/plugin/citation/view/resource/public/img/beautiful-image/generic/68043.png",1],
+          ["Sénèque","https://sf1.viepratique.fr/wp-content/uploads/sites/5/2019/01/citation-1-750x410.jpg",2],
+          ["Churchill","https://www.defi-ecologique.com/wp-content/uploads/citations-ecologie.jpg",3],
+          ["Salomé","http://img.over-blog-kiwi.com/1/58/44/60/20151218/ob_fcc5bc_la-vache-rose-jacques-salome-6.jpg",4]
+        ]
+      ],
+      [
+        ["Which quote defines you best ?"],[
+          ["Eau","https://10wallpaper.com/wallpaper/medium/1701/Scotland_Highlands_River-Winter_Landscape_HD_Wallpaper_medium.jpg",1],
+          ["Terre","https://cdn.futura-sciences.com/buildsv6/images/wide1920/8/a/3/8a3ab16898_106964_source-eau-potable.jpg",2],
+          ["Mer","https://i.pinimg.com/originals/61/0b/87/610b874b87156d674a5b4c116f929693.jpg",3],
+          ["Cascade","https://www.aeroportvoyage.com/wp-content/uploads/2019/07/eaux-thermales-tabacon-arenal-costa-rica-decouverte.jpg",4]
+        ]
+      ],
+      
     ],
     
     answerSet: [
